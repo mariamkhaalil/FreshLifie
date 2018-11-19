878941845fea4fa20454f9e30080d1200183dfa5
Mariam Khalil <mariamkhaalil@gmail.com>
2018-11-20 01:31:13 +0200

    init

---
+++ FreshLifie/Models/Provider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreshLifie.Models
+{
+    public class Provider
+    {
+        public int ProviderID { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderURL { get; set; }
+        // Foreign Key of Service Type Model
+        public int ServiceID { get; set; }
+
+    }
+}
---
+++ FreshLifie/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreshLifie.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPhone { get; set; }
+        public string UserEmail { get; set; }
+        public string UserAddress { get; set; }
+        public decimal UserLength { get; set; }
+        public decimal UserWeight { get; set; }
+        public string UserPassword { get; set; }
+
+
+    }
+}
