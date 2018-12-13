135025c6eab856885dc8c9472174f86870a565d6
mariamkhaalil <mariamkhaalil@gmail.com>
2018-12-13 23:16:21 +0200

    UploadTask5

--- FreshLifie/Startup.cs
+++ FreshLifie/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +34,7 @@ namespace FreshLifie
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<Data.FreshLifieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FreshLifieConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
